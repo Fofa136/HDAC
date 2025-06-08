ca64a70108f0ba67e24c07ac6de41127e32b0b09
Fofa136 <fadyhakem13@gmail.com>
2025-06-08 22:44:18 +0300

    PaDEL.sh

--- PaDEL.sh
+++ PaDEL.sh
@@ -1,1 +1,2 @@
 java -jar PaDEL-Descriptor.jar -fingerprints -descriptortypes descriptors_SubFP.xml -dir ./ -file descriptors_output_file.csv
+java -jar ./PaDEL-Descriptor.jar -file molecules.smi -descriptortypes descriptors_SubFP.xml ./descriptor.xml -d output.csv
